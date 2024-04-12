d8d723b48d30bc4d1b759a59fbb18109ff81a633
SzymonSergiusz <62117922+SzymonSergiusz@users.noreply.github.com>
2024-04-12 14:49:44 +0200

    +drugie zadanie

--- lab2/lab2_2.sh
+++ lab2/lab2_2.sh
@@ -14,11 +14,30 @@ if [ -d "${LIST_DIR}" ] && [ -d "${TARGET_DIR}" ]; then
     FILES=$(ls "$LIST_DIR")
 
     for FILE in ${FILES}; do
-        echo "$FILE"
-        if [[ -f "${LIST_DIR}/{$FILE}" ]]; then
-            echo ""
+
+        EXTENSION=${FILE##*.}
+        FILE_NAME=$(basename "$FILE" ."$EXTENSION")
+        FILE_NAME_UPPER=$(echo "$FILE_NAME" | tr '[:lower:]' '[:upper:]')
+
+        ABS_PATH=${PWD}
+
+        if [[ -f "${LIST_DIR}/${FILE}" ]]; then
+            echo "${LIST_DIR}/${FILE} - plik regularny"
+            ln -s "${FILE}" "${ABS_PATH}/${TARGET_DIR}/${FILE_NAME_UPPER}_ln.${EXTENSION}"
+
+        elif [[ -d "${LIST_DIR}/${FILE}" ]]; then
+            echo "${LIST_DIR}/${FILE} - katalog"
+            ln -s "${FILE}" "${ABS_PATH}/${TARGET_DIR}/${FILE_NAME_UPPER}_ln"
+
+        elif [[ -L "${LIST_DIR}/${FILE}" ]]; then
+            echo "${LIST_DIR}/${FILE} - dowiązanie symboliczne"
         fi
+
+
+
     done
+
+
 else
     echo "nie istnieją"
     exit 4
